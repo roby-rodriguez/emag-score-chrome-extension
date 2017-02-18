62090592b789ef03e7cda0bd02e003edd8353e55
John Doe <andrey.roba@gmail.com>
2017-02-18 19:07:38 +0200

    bugfix products filter

--- src/browser_action/util/filters.js
+++ src/browser_action/util/filters.js
@@ -1,6 +1,6 @@
 const filterBy = (list, property, value) =>
     list.filter(item =>
-        item[property].indexOf(value) > -1
+        item[property] && item[property].indexOf(value) > -1
     )
 
 export { filterBy }
